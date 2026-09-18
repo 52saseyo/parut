aa91ee6dffb45dd8f9cb7188869cbc7eecce0840
grrr-h <hong303047@gmail.com>
2026-09-18 12:10:54 +0900

    fix(product) : 기존 이미지 마이그레이션 백필 수정

--- product-service/src/main/resources/db/migration/image/V300__add_image_uploader_and_url.sql
+++ product-service/src/main/resources/db/migration/image/V300__add_image_uploader_and_url.sql
@@ -3,31 +3,16 @@ ALTER TABLE product_schema.p_images
     ADD COLUMN image_url VARCHAR(1000);
 
 UPDATE product_schema.p_images
-SET uploader_id = created_by::UUID
-WHERE uploader_id IS NULL
-  AND created_by ~* '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$';
+SET uploader_id = '00000000-0000-0000-0000-000000000000'
+WHERE uploader_id IS NULL;
 
 UPDATE product_schema.p_images
 SET image_url = 'https://example/' || image_key
 WHERE image_url IS NULL;
 
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM product_schema.p_images
-        WHERE uploader_id IS NULL
-           OR image_url IS NULL
-    ) THEN
-        RAISE EXCEPTION
-            'p_images uploader_id/image_url 백필에 실패한 행이 있습니다.';
-END IF;
-END
-$$;
-
 ALTER TABLE product_schema.p_images
     ALTER COLUMN uploader_id SET NOT NULL,
-ALTER COLUMN image_url SET NOT NULL;
+    ALTER COLUMN image_url SET NOT NULL;
 
 CREATE INDEX idx_p_images_uploader_id
     ON product_schema.p_images (uploader_id);
